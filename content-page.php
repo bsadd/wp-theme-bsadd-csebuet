c466a9a4e355833c8f44c24b0e80bb3fbd543f38
imran hira <imranhasanhira@gmail.com>
2017-05-06 11:15:10 +0200

    Added page template file to show only content for a page

---
+++ content-page.php
@@ -0,0 +1,12 @@
+<div class="blog-post">
+
+    <?php if ( has_post_thumbnail() ) {
+      the_post_thumbnail();
+    } ?>
+
+    <?php the_content(); ?>
+    <hr>
+
+<!-- the rest of the content -->
+
+</div><!-- /.blog-post -->
