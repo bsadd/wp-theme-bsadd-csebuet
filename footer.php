c522f5b2c86c41f457e5fa4fcc9ad993f153023c
imran hira <imranhasanhira@gmail.com>
2017-05-06 11:45:34 +0200

    added margin to footer and updated navigation active menu style

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 
-            <div class="row">
+            <div class="row footer-row">
                 <div class="col-xs-12 text-center">
                     <p class="small"> Theme by <a href="http://ihhira.com">Md Imran Hasan Hira</a> | Copyright 2017 @ BSADD | Powered by WordPress </p>
                 </div>
